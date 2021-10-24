d9e058871467b45413fd23d5afde5e99494b59f6
Savien <akawgan@gmail.com>
2021-10-24 19:25:48 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Twilight Tavern
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{backgroundColor: 'white', width: 50, height: 50}}></div>
+
       </header>
     </div>
   );
