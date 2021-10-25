79313f45f5f5d39964dde64bca67f2425022ed37
Savien <akawgan@gmail.com>
2021-10-25 19:13:56 +0200

    Modify the layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 
 function App() {
   return (
+    <div className="App-container">
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Twilight Tavern
+          Header
         </p>
         <div style={{backgroundColor: 'black', width: 50, height: 50}}></div>
 
       </header>
+      <section className="App-section">
+        <p>
+          Main Section
+        </p>
+      </section>
     </div>
+</div>
   );
 }
 
